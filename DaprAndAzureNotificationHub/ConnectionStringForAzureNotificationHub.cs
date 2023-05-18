2c3b5711b475ba08fcafe7d0dabc06242843855a
Sergio Parra Guerra <sparra@encamina.com>
2023-05-18 12:47:00 +0200

    first commit

---
+++ DaprAndAzureNotificationHub/ConnectionStringForAzureNotificationHub.cs
@@ -0,0 +1,18 @@
+﻿namespace DaprAndAzureNotificationHub;
+
+public class ConnectionStringForAzureNotificationHub
+{
+    public ConnectionStringForAzureNotificationHub(string endpoint, string hubName, string sasKeyName, string sasKeyValue)
+    {
+        Endpoint = endpoint;
+        HubName = hubName;
+        SasKeyName = sasKeyName;
+        SasKeyValue = sasKeyValue;
+    }
+
+    public string Endpoint { get; set; } = string.Empty;
+    public string HubName { get; set; } = string.Empty;
+    public string SasKeyName { get; set; } = string.Empty;
+    public string SasKeyValue { get; set; } = string.Empty;
+    public string GetNotificationHubUri() => $"{Endpoint}{HubName}{CoreConstants.PUSH_NOTIFICATION_URI_PATH}";
+}
