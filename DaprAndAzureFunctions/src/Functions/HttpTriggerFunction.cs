6be35a78a6db7be79b647e8e9e37c5dbca9b85dd
Sergio Parra Guerra <sparra@encamina.com>
2023-06-01 19:12:14 +0200

    openapi

--- DaprAndAzureFunctions/src/Functions/HttpTriggerFunction.cs
+++ DaprAndAzureFunctions/src/Functions/HttpTriggerFunction.cs
@@ -10,6 +10,10 @@ using Newtonsoft.Json;
 using Microsoft.Azure.WebJobs.Extensions.Dapr;
 using System.Text.Json;
 using Microsoft.Azure.Functions.Extensions.Dapr.Core;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
+using System.Net;
 
 namespace DaprAndAzureFunctions.Functions;
 
@@ -24,6 +28,10 @@ public class HttpTriggerFunction
     }
 
     [FunctionName(nameof(SendMessageToQueue))]
+    [OpenApiOperation(operationId: "SendMessageToQueue", tags: new[] { "SendMessageToQueue" })]
+    [OpenApiSecurity("code", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
     public async Task<IActionResult> SendMessageToQueue(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
         [DaprBinding(BindingName = "storage-queue", Operation = "create")] IAsyncCollector<JsonElement> messages)
